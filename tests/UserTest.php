c23cd33e18db4dd44ff6ef8fbadb644414fd954f
manantech <sujay.ghosh@lykapp.com>
2019-12-08 00:50:12 +0530

    mock object

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -53,4 +53,17 @@ class UserTest extends  TestCase {
     }
 
 
+    public function testCannotNotifyUserWithNoEmail() {
+        $user = new User;
+
+        //$mock_mailer = $this->createMock(Mailer::class);
+
+        //$mock_mailer->method('sendMessage')->will($this->throwException(new Exception));
+        $user->setMailer($mock_mailer);
+        
+        $user->notify("hello");
+        $this->expectException(Exception::class);
+    }
+
+
 }
