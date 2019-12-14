c12938ecdfe2e3709b2d6878bc8fd1df29ba739a
mantech <ghoshsujay95@gmail.com>
2019-12-14 22:36:01 +0530

    create mock

---
+++ tests/OrderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+
+class OrderTest extends TestCase{
+    public function testOrderIsProcessed(){
+        $gateway = $this->createMock('PaymentGateway');
+        $order = new Order($gateway);
+        $order->amount = 200;
+        $this->assertTrue($order->process());
+
+    }
+}
